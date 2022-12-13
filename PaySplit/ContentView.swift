181301f38fde8333b9c99e547086bfb7c4f5cd85
Wiljo de Ruiter <wiljo.de.ruiter@ziggo.nl>
2022-12-13 22:12:26 +0100

    Add completed view

--- PaySplit/ContentView.swift
+++ PaySplit/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        return checkAmount + tipValue
+    }
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    var totalPerCouple: Double {
+        return 2 * totalPerPerson
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount,
+                              format: .currency( code: Locale.current.currency?.identifier ?? "EUR"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                            ForEach(2 ..< 40) {
+                                Text("\($0) people")
+                            }
+                        }
+                } header: {
+                    Text("Enter the value to split")
+                }
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach( 0 ..< 25 ) {
+                            Text( $0, format: .percent )
+                        }
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                Section {
+                    Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                } header: {
+                    Text("Grand total")
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                } header: {
+                    Text("For each person")
+                }
+                Section {
+                    Text(totalPerCouple, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                } header: {
+                    Text("For each couple")
+                }
+            }
+            .navigationTitle("Pay Split")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
