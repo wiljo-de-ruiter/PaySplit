c0271119dfc28e2ea021fdeb92b9464c55f2521c
Wiljo de Ruiter <wiljo.de.ruiter@ziggo.nl>
2022-12-13 20:27:21 +0100

    Initial Commit

---
+++ PaySplit/PaySplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaySplitApp.swift
+//  PaySplit
+//
+//  Created by Wiljo de Ruiter on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PaySplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
